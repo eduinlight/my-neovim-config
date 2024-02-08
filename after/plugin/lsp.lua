9ba9ec80ca2fe361ca10d40f2eef9bdb26121ae5
Eduin Garcia Cordero <eduinlight@gmail.com>
2024-02-08 12:09:35 +0100

    save

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -88,6 +88,8 @@ cmp.setup({
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'path' },
+    { name = 'buffer' },
   },
   mapping = {
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
